9f0bf73bd9b498f300f9b13209b3cd20a9ec7d04
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-11-02 23:56:37 +0000

    Case 1 (inferred includes Action): print baseline BNT fusion

--- src/matlab/inference_incl_action.m
+++ src/matlab/inference_incl_action.m
@@ -89,3 +89,16 @@ result = normalize(result);
 
 fprintf('\n... result (normalized) =\n');
 disp(result);
+
+%% BNT soft evidence
+netobj2 = netobj;
+netobj2 = BNResetEvidence(netobj2);
+netobj2 = BNEnterNodeEvidence(netobj2, observed, true, {'Action', hmm_ev});
+pred2 = BNSoftPredictionAccuracy3(netobj2, inferred);
+fprintf('Using BNT soft evidence, P_BN =\n');
+disp(pred2.T);
+%this warning is printed also when the matrices seem equal - maybe some
+%slight decimal values differ?
+% if result ~= pred2.T
+%     warning('inference_incl_action: our result differs from BNT''s result!');
+% end;
