ddf3bc544eda70b1f3c8329d2d9a31115ea4dd11
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-11-02 23:43:05 +0000

    main.m: streamline

--- src/matlab/main.m
+++ src/matlab/main.m
@@ -12,11 +12,16 @@ configurePaths;
 %% load Bayesian Network
 load('BN_lab.mat');
 
-%% examples
-e = 1; % example counter
-
 %% retain evidence?
 incremental = false;
+
+%% Action evidence from GestureHMM
+hmm_ev = [0.8 0.1 0.1]; % HMM evidence in GestureHMM order: tap,grasp,touch
+reorder = [2 1 3]; % TODO use get_remapping
+hmm_ev_ordered = hmm_ev(:, reorder); % BNActionValue order: grasp,tap,touch
+
+%% example counter
+e = 1;
 
 fprintf('case 1 examples (inference over nodes including Action)\n\n');
 % =====================================================================
@@ -25,14 +30,9 @@ fprintf('%d.\n', e);
 
 observed = {'Shape', 'circle', 'Size', 'small'}; % BN observed nodes
 inferred = {'Action'}; % BN nodes to infer
-hmm_ev = [0.8 0.1 0.1]; % HMM evidence in GestureHMM order
-% GestureHMM order: tap, grasp, touch
-% BNActionValue order: grasp, tap, touch
-reorder = [2 1 3]; % TODO use get_remapping
-hmm_ev_ordered = hmm_ev(:, reorder); % HMM evidence in BNActionValue order
 fusion(netobj_lab, inferred, observed, hmm_ev_ordered, incremental);
 
-clear observed inferred hmm_ev reorder hmm_ev_ordered result;
+clear observed inferred;
 e = e+1;
 
 fprintf('%d.\n', e);
@@ -40,48 +40,36 @@ fprintf('%d.\n', e);
 % Word nodes: repeat it to indicate presence, set to '-' to indicate absense
 observed = {'Shape', 'circle', 'Size', 'small', 'moves', 'moves'};
 inferred = {'Action'};
-hmm_ev = [0.8 0.1 0.1];
-reorder = [2 1 3]; % TODO use get_remapping
-hmm_ev_ordered = hmm_ev(:, reorder);
 fusion(netobj_lab, inferred, observed, hmm_ev_ordered, incremental);
 
-clear observed inferred hmm_ev reorder hmm_ev_ordered result;
+clear observed inferred;
 e = e+1;
 
 fprintf('%d.\n', e);
 
 observed = {'Shape', 'circle', 'Size', 'small', 'moves', 'moves', 'rolling', '-'};
 inferred = {'Action'};
-hmm_ev = [0.8 0.1 0.1];
-reorder = [2 1 3]; % TODO use get_remapping
-hmm_ev_ordered = hmm_ev(:, reorder);
 fusion(netobj_lab, inferred, observed, hmm_ev_ordered, incremental);
 
-clear observed inferred hmm_ev reorder hmm_ev_ordered result;
+clear observed inferred;
 e = e+1;
 
 fprintf('%d.\n', e);
 
 observed = {'Color', 'yellow', 'Shape', 'circle'};
 inferred = {'Action', 'ObjVel'};
-hmm_ev = [0.8 0.1 0.1];
-reorder = [2 1 3]; % TODO use get_remapping
-hmm_ev_ordered = hmm_ev(:, reorder);
 fusion(netobj_lab, inferred, observed, hmm_ev_ordered, incremental);
 
-clear observed inferred hmm_ev reorder hmm_ev_ordered result;
+clear observed inferred;
 e = e+1;
 
 fprintf('%d.\n', e);
 
 observed = {'ObjVel', 'fast', 'Shape', 'circle'};
 inferred = {'Action', 'Color'};
-hmm_ev = [0.8 0.1 0.1];
-reorder = [2 1 3]; % TODO use get_remapping
-hmm_ev_ordered = hmm_ev(:, reorder);
 fusion(netobj_lab, inferred, observed, hmm_ev_ordered, incremental);
 
-clear observed inferred hmm_ev reorder hmm_ev_ordered result;
+clear observed inferred;
 e = e+1;
 
 fprintf('case 2 examples (inference over nodes not including Action)\n\n');
@@ -91,24 +79,18 @@ fprintf('%d.\n', e);
 
 observed = {'Shape', 'circle'};
 inferred = {'Color'};
-hmm_ev = [0.8 0.1 0.1];
-reorder = [2 1 3]; % TODO use get_remapping
-hmm_ev_ordered = hmm_ev(:, reorder);
 fusion(netobj_lab, inferred, observed, hmm_ev_ordered, incremental);
 
-clear observed inferred hmm_ev reorder hmm_ev_ordered result;
+clear observed inferred;
 e = e+1;
 
 fprintf('%d.\n', e);
 
 observed = {'Shape', 'circle', 'Size', 'small'};
 inferred = {'Color'};
-hmm_ev = [0.8 0.1 0.1];
-reorder = [2 1 3]; % TODO use get_remapping
-hmm_ev_ordered = hmm_ev(:, reorder);
 fusion(netobj_lab, inferred, observed, hmm_ev_ordered, incremental);
 
-clear observed inferred hmm_ev reorder hmm_ev_ordered result;
+clear observed inferred;
 e = e+1;
 
 % TODO: support case-2 queries with more than one inferred nodes,
