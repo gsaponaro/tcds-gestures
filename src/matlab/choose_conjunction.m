bd72e4fbdc62c76fc2fb257a9619395a622c3533
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-11-05 20:09:06 +0000

    Add plots for conjunction experiment (and/but)

--- src/matlab/choose_conjunction.m
+++ src/matlab/choose_conjunction.m
@@ -4,6 +4,10 @@ configurePaths;
 %% load data
 load('BN_lab.mat');
 
+%% user parameters
+word_threshold = 0.2;
+create_figures = true;
+
 % 1000 sentences generated randomly accroding to the grammar in
 % AffordanceAndSpeech/word2sent/grammar.grm (see Makefile for more
 % information)
@@ -21,6 +25,20 @@ fprintf('%d.\n', e);
 %% enter some evidence
 netobj_lab = BNEnterNodeEvidence(netobj_lab, ...
     {'Size', 'small', 'Shape', 'box', 'Action', 'touch', 'Contact', 'long', 'ObjHandVel', 'slow'});
+
+%% extract word probabilities
+pw = BNGetWordProbs(netobj_lab);
+
+toplot = pw>word_threshold;
+if create_figures
+    figure;
+    bar(pw(toplot));
+    set(gca, 'xtick', 1:length(netobj_lab.WORDNODES(toplot)));
+    set(gca, 'xticklabel', netobj_lab.nodeNames(netobj_lab.WORDNODES(toplot)));
+    set(gca,'XTickLabelRotation',45);
+    ylabel('$p(w_i)$', 'Interpreter','latex', 'FontSize', 20);
+    print('-depsc', 'and_pw.eps');
+end;
 
 %% rescore sentences with word probabilities and pick best sentence
 [sentences, normlogprobs] = BNScoreSentences(netobj_lab, sentenceFilename);
@@ -50,6 +68,18 @@ fprintf('%d.\n', e);
 
 netobj_lab = BNEnterNodeEvidence(netobj_lab, ...
     {'Size', 'small', 'Shape', 'box', 'Action', 'touch', 'Contact', 'short', 'ObjHandVel', 'medium'});
+
+pw = BNGetWordProbs(netobj_lab);
+toplot = pw>word_threshold;
+if create_figures
+    figure;
+    bar(pw(toplot));
+    set(gca, 'xtick', 1:length(netobj_lab.WORDNODES(toplot)));
+    set(gca, 'xticklabel', netobj_lab.nodeNames(netobj_lab.WORDNODES(toplot)));
+    set(gca,'XTickLabelRotation',45);
+    ylabel('$p(w_i)$', 'Interpreter','latex', 'FontSize', 20);
+    print('-depsc', 'but_pw.eps');
+end;
 
 [sentences, normlogprobs] = BNScoreSentences(netobj_lab, sentenceFilename);
 
