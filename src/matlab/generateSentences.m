e9d9b1caf537174619bb60c1d883a1d04a69df76
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-11-08 00:10:38 +0000

    Example generated sentences: also show p(word)

--- src/matlab/generateSentences.m
+++ src/matlab/generateSentences.m
@@ -7,6 +7,10 @@ configurePaths;
 %% load data
 load('BN_lab.mat');
 
+%% user parameters
+word_threshold = 0.2;
+create_figures = true;
+
 % 1000 sentences generated randomly accroding to the grammar in
 % AffordanceAndSpeech/word2sent/grammar.grm (see Makefile for more
 % information)
@@ -16,6 +20,20 @@ sentenceFilename = 'sentence_data/sentence_1000samples.txt'
 %% enter some evidence
 netobj_lab = BNEnterNodeEvidence(netobj_lab, {'Color', 'yellow', ...
     'Size', 'big', 'Shape', 'circle', 'ObjVel', 'fast'});
+
+%% extract word probabilities
+pw = BNGetWordProbs(netobj_lab);
+
+toplot = pw>word_threshold;
+if create_figures
+    figure;
+    bar(pw(toplot));
+    set(gca, 'xtick', 1:length(netobj_lab.WORDNODES(toplot)));
+    set(gca, 'xticklabel', netobj_lab.nodeNames(netobj_lab.WORDNODES(toplot)));
+    set(gca,'XTickLabelRotation',45);
+    ylabel('$p(w_i)$', 'Interpreter','latex', 'FontSize', 20);
+    print('-depsc', 'example_pw.eps');
+end;
 
 %% rescore sentences with word probabilities and pick best sentence
 [sentences, normlogprobs] = BNScoreSentences(netobj_lab, sentenceFilename);
