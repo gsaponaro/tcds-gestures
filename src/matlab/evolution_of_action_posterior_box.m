207dd39e3730af0ee74e8a793bfc3cfa72e3d01c
Giampiero Salvi <giampi@kth.se>
2017-11-11 16:27:57 +0100

    simplified code

--- src/matlab/evolution_of_action_posterior_box.m
+++ src/matlab/evolution_of_action_posterior_box.m
@@ -120,44 +120,38 @@ human_cell = separate_sequence(human, human_seglim);
 %% transpose to BNT format
 % i.e. from data{example}(frame,coord) to data{example}(coord,frame)
 human_BNT = transpose_cell_array(human_cell);
+human_data = human_BNT{1}; % get the matrix of coordinates
 
 %% run Forward-Backward on increasingly complete data
 interval = 5;
-sequence_length = human_endframe-human_startframe-shrink*2;
-num_iterations = floor(sequence_length/interval);
+%sequence_length = human_endframe-human_startframe-shrink*2;
+sequence_length = size(human_data, 2);
+iteration_frames = interval:interval:sequence_length;
+num_iterations = length(iteration_frames);
 ev = cell(1,num_iterations); % will store HMM evidence in BNActionValue order: grasp,tap,touch
-for frames_so_far = 5:interval:human_endframe
+for iter_fb = 1:num_iterations
+    frames_so_far = iteration_frames(iter_fb);
+    human_so_far = human_data(:, 1:frames_so_far);
 
-    iter_fb = frames_so_far / interval;
-    
-    if frames_so_far > sequence_length
-        break;
-    end;
-
-    human_so_far = human_BNT{1}(:, 1:frames_so_far);
-
-    scores_map = containers.Map;
+    %scores_map = containers.Map;
 
     %fprintf('===============================================================\n\n');
     %fprintf('test data: human, subsequence: %d, ground truth: 1 (tap)\n', ex);
-    scores_map('tap') = mhmm_logprob(human_so_far, hmm1_prior, hmm1_trans, hmm1_mu, hmm1_Sigma, hmm1_mixmat);
-    scores_map('grasp') = mhmm_logprob(human_so_far, hmm3_prior, hmm3_trans, hmm3_mu, hmm3_Sigma, hmm3_mixmat);
-    scores_map('push') = mhmm_logprob(human_so_far, hmm4_prior, hmm4_trans, hmm4_mu, hmm4_Sigma, hmm4_mixmat);
+    tap_score = mhmm_logprob(human_so_far, hmm1_prior, hmm1_trans, hmm1_mu, hmm1_Sigma, hmm1_mixmat);
+    grasp_score = mhmm_logprob(human_so_far, hmm3_prior, hmm3_trans, hmm3_mu, hmm3_Sigma, hmm3_mixmat);
+    push_score = mhmm_logprob(human_so_far, hmm4_prior, hmm4_trans, hmm4_mu, hmm4_Sigma, hmm4_mixmat);
     %scores_map('garbage') = mhmm_logprob(human_so_far, hmm0_prior, hmm0_trans, hmm0_mu, hmm0_Sigma, hmm0_mixmat);
-    k = keys(scores_map); % alphabetical order: grasp,push,tap
-    v = values(scores_map);
-    v2 = cell2mat(v);
-    logsum = logsumexp(v2);
-    v_norm = exp(v2-logsum);
-
-    v_norm = round(v_norm); % get rid of minuscule numbers
-    
-    v_norm_reordered = v_norm(:, [1 3 2]); % now grasp,tap,push which we treat as grasp,tap,touch
+    %k = keys(scores_map); % alphabetical order: grasp,push,tap
+    %v = values(scores_map);
+    k = {'grasp', 'tap', 'push'};
+    v2 = [grasp_score, tap_score, push_score];
+    v_norm = exp(v2-logsumexp(v2));
+    v_norm = v_norm/sum(v_norm); % this is just to eliminate numerical problems
     
     % save the evidence pdf for later plots
-    ev{iter_fb} = v_norm_reordered;
+    ev{iter_fb} = v_norm; % not sure if this is needed
 
-    [m,i] = max(cell2mat(values(scores_map)));
+    [m,i] = max(v_norm);
     fprintf('observing %d frames, pdf: [%s] -> winner %d (%s)\n', frames_so_far, num2str(v_norm), i, k{i});
 
 end;
@@ -181,17 +175,19 @@ for iter_bn = 1:num_iterations
 
 end;
 
+%%
+
 if create_figures
 
 %     list_of_ev_as_strings = cell(1,num_iterations);
-     list_of_real_frames = cell(1,1+num_iterations);
-     list_of_real_frames{1} = mat2str(1);
-     for iter_ev = 1:num_iterations
+%     list_of_real_frames = cell(1,1+num_iterations);
+%     list_of_real_frames{1} = mat2str(1);
+%     for iter_ev = 1:num_iterations
 %         list_of_ev_as_strings{iter_ev} = mat2str(ev{iter_ev});
-         iter_ev;
-         list_of_real_frames{1+iter_ev} = mat2str(iter_ev*interval*2);
-     end;
-%     
+%         iter_ev;
+%         list_of_real_frames{1+iter_ev} = mat2str(iter_ev*interval*2);
+%     end;
+
 %     figure;
 %     b = bar(cell2mat(p)');
 %     xlabel('Action Evidence [grasp tap touch]', 'FontSize',fontsize);
@@ -207,27 +203,27 @@ if create_figures
     ev_grasp = ev_all(:,1);
     ev_tap = ev_all(:,2);
     ev_touch = ev_all(:,3);
-    hp = plot(1:num_iterations,ev_grasp, 1:num_iterations,ev_tap, 1:num_iterations,ev_touch);
+    hp = plot(iteration_frames, ev_grasp, iteration_frames,ev_tap, iteration_frames,ev_touch);
     hp(1).Color = 'g';
-    hp(1).LineStyle = ':';
-    hp(1).LineWidth = 5;
-    hp(1).Marker = '*';
+    hp(1).LineStyle = '--';
+    hp(1).LineWidth = 1;
+    %hp(1).Marker = '*';
     hp(2).Color = 'r';
     hp(2).LineStyle = '-';
-    hp(2).LineWidth = 2;
-    hp(2).Marker = '+';
+    hp(2).LineWidth = 1;
+    %hp(2).Marker = '+';
     hp(3).Color = 'b';
     hp(3).LineStyle = '-.';
-    hp(3).LineWidth = 2;
-    hp(3).Marker = 'x';
-    xlabel('$\rm{frame~N}~(\times~30~\rm{ms})$', 'Interpreter','latex', 'FontSize',fontsize);
-    set(gca, 'xticklabels', list_of_real_frames);
+    hp(3).LineWidth = 1;
+    %hp(3).Marker = 'x';
+    xlabel('$\rm{frame~k}~(\times~30~\rm{ms})$', 'Interpreter','latex', 'FontSize',fontsize);
+    %set(gca, 'xticklabels', list_of_real_frames);
     %set(gca, 'xticklabels',{[]});
-    set(gca, 'ylim', [-0.05 1.05]);
+    set(gca, 'ylim', [-0.05 1.05], 'xlim', [1 sequence_length]);
     %ylabel('$P_{\rm{HMM}}(\rm{Action}=a_k \mid G_1^k)$', 'Interpreter','latex', 'FontSize',fontsize);
-    ylabel('$P_{\rm{HMM}}(A \mid G_1^N)$', 'Interpreter','latex', 'FontSize',fontsize);
+    ylabel('$P_{\rm{HMM}}(A \mid G_1^k)$', 'Interpreter','latex', 'FontSize',fontsize);
     l2 = legend(hp, 'grasp', 'tap', 'touch');
-    l2.FontSize = fontsize-1;
+    l2.FontSize = fontsize;
     l2.Location = 'east';
     print('-depsc', 'evolution_of_action_posterior_on_box.eps');
 end;
