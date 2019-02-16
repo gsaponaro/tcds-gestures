7dcb6bd5d71011f65bfdb63d18283b7cdf7acb87
Giovanni Saponaro <gsaponaro@isr.tecnico.ulisboa.pt>
2019-02-16 17:53:54 +0000

    Update copyright, contact, license

--- src/humanHandProcessor/src/HumanHandProcessorMain.cpp
+++ src/humanHandProcessor/src/HumanHandProcessorMain.cpp
@@ -1,8 +1,8 @@
 /*
- * Copyright: (C) 2017 VisLab, Institute for Systems and Robotics,
+ * Copyright: (C) 2019 VisLab, Institute for Systems and Robotics,
  *                     Istituto Superior Técnico, Lisbon, Portugal
- * Author: Giovanni Saponaro
- * CopyPolicy: Released under the terms of the GNU GPL v2.0
+ * Author: Giovanni Saponaro <gsaponaro@isr.tecnico.ulisboa.pt>
+ * CopyPolicy: Released under the terms of the GNU GPL v3.0
  */
 
 #include <yarp/os/Network.h>
